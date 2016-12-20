f21d2481751270ccad69716f6daafda784ca52a6
Mrutyunjay-acadgild <nayak.mrutyunjay3@gmail.com>
2016-12-20 23:10:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Mrutyunjay Nayak";
+var age = 23;
+var dateofBirth = "07-07-1993";
+var placeOfBirth ="Jajpur";
+
+console.log("Name :"+name);
+console.log("Age :"+age);
+console.log("Date of Birth :"+dateofBirth);
+console.log("Place of Birth :"+placeOfBirth);
